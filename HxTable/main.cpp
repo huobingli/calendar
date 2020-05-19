55e2d65516b619b347a97e4121b8a7f1b4deb78e
huobingli <huobingli@outlook.com>
2020-05-19 23:59:55 +0800

    update

---
+++ HxTable/main.cpp
@@ -0,0 +1,33 @@
+#include <windows.h>
+#include <objbase.h>
+#include <ATLComTime.h>
+
+#ifdef _DEBUG
+#ifdef _UNICODE
+#pragma comment(lib,"..//bin//DuiLib_ud.lib")
+#else
+//#pragma comment(lib,"..//bin//DuiLib_d.lib")
+#pragma comment(lib,"..//ListDemo//bin//DuiLib_d.lib")
+#endif
+#else
+#ifdef _UNICODE
+#pragma comment(lib,"..//bin//DuiLib_u.lib")
+#else
+#pragma comment(lib,"..//bin//DuiLib.lib")
+#endif
+#endif
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*lpCmdLine*/, int nCmdShow)
+{
+	CPaintManagerUI::SetInstance(hInstance);
+	CPaintManagerUI::SetResourcePath(CPaintManagerUI::GetInstancePath());
+
+
+// 	HxCalendarWnd *pFrame = new HxCalendarWnd;
+// 	pFrame->CreateDuiWindow(NULL, _T("ÈÕÀú²âÊÔ"), UI_WNDSTYLE_DIALOG, 0L);
+// 	pFrame->CenterWindow();
+
+	CPaintManagerUI::MessageLoop();
+	::CoUninitialize();
+	return 0;
+}
